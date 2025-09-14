634a6dabd3b43f8c98e1320797f555eea93176b7
hi-rachel <rachel.uiux@gmail.com>
2025-09-14 22:47:20 +0900

    fix: vitest, e2e 테스트 실행 분리

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, defaultExclude } from "vitest/config";
 import path from "path";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./src/test/setup.ts"],
+    // 기본 제외값 + E2E 폴더만 추가 제외
+    exclude: [...defaultExclude, "tests/**", "playwright.config.ts"],
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
